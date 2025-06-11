a7a54cc401bcf7275d01fe641f4cf306d773799f
Julius-wol <nkhuyen300305@gmail.com>
2025-06-11 15:18:42 +0700

    Video 60

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,32 @@
+package vn.hoidanit.laptopshop.controller;
+
+import vn.hoidanit.laptopshop.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/admin/user")
+    public String getHomePage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User khanhhuyen) {
+        System.out.println(khanhhuyen);
+        this.userService.handleSaveUser(khanhhuyen);
+        return "hello";
+    }
+}
